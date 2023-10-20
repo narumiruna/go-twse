b7fe055f3f33e7c25010bb794520aa4856ea4c87
narumi <toucans-cutouts0f@icloud.com>
2023-10-20 20:07:45 +0800

    fix trade price

--- pkg/twse/types.go
+++ pkg/twse/types.go
@@ -30,84 +30,62 @@ type QueryTime struct {
 }
 
 type StockInfo struct {
-	Volume      string `json:"tv"`
-	Ps          string `json:"ps"`
-	Nu          string `json:"nu"`
-	Pz          string `json:"pz"`
-	Bp          string `json:"bp"`
-	Fv          string `json:"fv"`
-	Oa          string `json:"oa"`
-	Ob          string `json:"ob"`
-	A           string `json:"a"`
-	B           string `json:"b"`
-	Symbol      string `json:"c"`
-	D           string `json:"d"`
-	Ch          string `json:"ch"`
-	Ot          string `json:"ot"`
-	UpdatedAt   string `json:"tlong"`
-	F           string `json:"f"`
-	IP          string `json:"ip"`
-	G           string `json:"g"`
-	Mt          string `json:"mt"`
-	Ov          string `json:"ov"`
-	High        string `json:"h"`
-	It          string `json:"it"`
-	Oz          string `json:"oz"`
-	Low         string `json:"l"`
-	ShortName   string `json:"n"`
-	Open        string `json:"o"`
-	P           string `json:"p"`
-	Ex          string `json:"ex"`
-	S           string `json:"s"`
-	T           string `json:"t"`
-	U           string `json:"u"`
-	TotalVolume string `json:"v"`
-	W           string `json:"w"`
-	Name        string `json:"nf"`
-	PrevClose   string `json:"y"`
-	Last        string `json:"z"`
-	Ts          string `json:"ts"`
+	Symbol    string `json:"c"`
+	Ticker    string `json:"ch"`
+	Name      string `json:"nf"`
+	ShortName string `json:"n"`
+	Exchange  string `json:"ex"` // 上市或上櫃
+
+	Ask        float64 `json:"oa,string"` // 賣價
+	Bid        float64 `json:"ob,string"` // 買價
+	Asks       string  `json:"a"`         // 五檔賣出價格
+	Bids       string  `json:"b"`         // 五檔買入價格
+	AskVolumes string  `json:"f"`         // 五檔賣出數量
+	BidVolumes string  `json:"g"`         // 五檔買入數量
+
+	PrevClose float64 `json:"y,string"` // 昨收
+	Open      float64 `json:"o,string"` // 開盤
+	High      float64 `json:"h,string"` // 最高
+	Low       float64 `json:"l,string"` // 最低
+	Close     float64 `json:"z,string"` // 收盤
+
+	TradePrice        float64 `json:"pz,string"` // 成交價
+	Volume            int64   `json:"tv,string"` // 成交量
+	AccumulatedVolume int64   `json:"v,string"`  // 累積成交量
+
+	LimitUp   string `json:"u"` // 漲停價
+	LimitDown string `json:"w"` // 跌停價
+
+	UpdatedAt string `json:"t"`
+	Timestamp int64  `json:"tlong,string"`
+
+	TradeDate string `json:"d"` // 最近交易日期
+
+	Ps string `json:"ps"`
+	Nu string `json:"nu"`
+	Bp string `json:"bp"`
+	Fv string `json:"fv"`
+	Ot string `json:"ot"`
+	IP string `json:"ip"`
+	Mt string `json:"mt"`
+	Ov string `json:"ov"`
+	It string `json:"it"`
+	Oz string `json:"oz"`
+	P  string `json:"p"`
+	S  string `json:"s"`
+	Ts string `json:"ts"`
 }
 
 func (i StockInfo) String() string {
-	last, err := strconv.ParseFloat(i.Last, 64)
-	if err != nil {
-		last = 0
-	}
-
-	prevClose, err := strconv.ParseFloat(i.PrevClose, 64)
-	if err != nil {
-		prevClose = 0
-	}
-
-	open, err := strconv.ParseFloat(i.Open, 64)
-	if err != nil {
-		open = 0
-	}
-
-	high, err := strconv.ParseFloat(i.High, 64)
-	if err != nil {
-		high = 0
-	}
-
-	low, err := strconv.ParseFloat(i.Low, 64)
-	if err != nil {
-		low = 0
-	}
-
-	netChange := (last/prevClose - 1.0) * 100
-	if last == 0 || prevClose == 0 {
-		netChange = 0.0
-	}
-
-	return fmt.Sprintf("%s(%s), Open: %s, High: %s, Low: %s, Last: %s, Net Change: %.2f%%, Volume: %s",
+	netChange := (i.TradePrice/i.PrevClose - 1.0) * 100
+	return fmt.Sprintf("%s(%s), Open: %s, High: %s, Low: %s, Last: %s, Net Change: %.2f%%, Volume: %d",
 		i.ShortName,
 		i.Symbol,
-		humanize.Commaf(open),
-		humanize.Commaf(high),
-		humanize.Commaf(low),
-		humanize.Commaf(last),
+		humanize.Commaf(i.Open),
+		humanize.Commaf(i.High),
+		humanize.Commaf(i.Low),
+		humanize.Commaf(i.Close),
 		netChange,
-		i.TotalVolume,
+		i.AccumulatedVolume,
 	)
 }
